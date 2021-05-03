911eb7c5c659ca636f122d0c0417f340677fe20d
SY-Mina <sylim9387@naver.com>
2021-05-03 19:32:14 +0900

    Edit Layout

--- src/main/java/com/example/wheretowatch/SearchFragment.java
+++ src/main/java/com/example/wheretowatch/SearchFragment.java
@@ -1,12 +1,17 @@
 package com.example.wheretowatch;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatImageButton;
 import androidx.fragment.app.Fragment;
 
 public class SearchFragment extends Fragment {
@@ -15,5 +20,29 @@ public class SearchFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_search, container, false);
         //git test용 주석
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        EditText searchText = (EditText)view.findViewById(R.id.etSearch);
+        AppCompatImageButton searchBtn = (AppCompatImageButton)view.findViewById(R.id.btnSearch);
+        AppCompatButton eraseBtn = (AppCompatButton)view.findViewById(R.id.btnErase);
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String searchString = searchText.getText().toString();
+                Log.d("search", searchString);
+            }
+        });
+
+        eraseBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                searchText.setText("");
+            }
+        });
     }
 }
